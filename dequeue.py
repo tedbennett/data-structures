632852ed10d1c27e2d5dce90f7ef7d9824eb9944
tedbennett <teddsbennett@gmail.com>
2020-01-25 20:09:38 +0000

    deque adding items unit test

--- dequeue.py
+++ dequeue.py
@@ -6,6 +6,18 @@ class Dequeue(CircularQueue):
     def __init__(self, length):
         super().__init__(length)
 
+    def add_first(self, item):
+        if self.len >= self.max_size:
+            raise Exception("Dequeue is full")
+        self.zero_index -= 1
+        if self.zero_index < 0:
+            self.zero_index = self.max_size + self.zero_index
+        self._queue[self.zero_index] = item
+        self.len += 1
+
+    def add_last(self, item):
+        super().enqueue(item)
+
     def first(self):
         super().first()
 
@@ -28,6 +40,15 @@ class DequeueTest(unittest.TestCase):
             queue.first()
         with self.assertRaises(Exception):
             queue.last()
+
+    def test_add(self):
+        queue = Dequeue(20)
+        for i in range(10):
+            queue.add_last(i)
+        for i in range(10, 20):
+            queue.add_first(i)
+        with self.assertRaises(Exception):
+            queue.add_last(1)
 
 
 if __name__ == "__main__":
