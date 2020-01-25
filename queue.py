ca8c07654f451a96f9ebc83214d876074c302e54
tedbennett <teddsbennett@gmail.com>
2020-01-25 18:12:02 +0000

    circular queue implementation

--- queue.py
+++ queue.py
@@ -27,3 +27,37 @@ class QueueBase:
 
     def __len__(self):
         return self.len
+
+
+class CircularQueue(QueueBase):
+    def __init__(self, max_size):
+        super().__init__()
+        self.max_size = max_size
+        self._queue = [None] * self.max_size
+
+    def enqueue(self, item):
+        if self.len >= self.max_size:
+            raise Exception("Queue is full")
+        index = (self.zero_index + self.len) % self.max_size  # finds end of queue, checks if needs to wrap around
+        self._queue[index] = item
+        self.len += 1
+
+    def dequeue(self):
+        if self.len == 0:
+            raise Exception("Queue is empty")
+        self.len -= 1
+        item = self._queue[self.zero_index]
+        self._queue[self.zero_index] = None
+        self.zero_index = (self.zero_index + 1) % self.max_size
+        return item
+
+    def first(self):
+        return super().first()
+
+    def is_empty(self):
+        return super().is_empty()
+
+    def __len__(self):
+        return super().__len__()
+
+
