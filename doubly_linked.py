cab1bb046e3aa8450f13edae21d6f8faac28379f
ted <teddsbennett@gmail.com>
2020-01-26 17:55:57 +0000

    update double linked test to include more general insert and remove functions

--- doubly_linked.py
+++ doubly_linked.py
@@ -5,12 +5,12 @@ from linked_list import LinkedListBase
 class DoublyLinkedList(LinkedListBase):
     def __init__(self):
         super().__init__()
-        self.head_sentinel = self.Node()
-        self.tail_sentinel = self.Node()
-        self.head_sentinel.next = self.tail_sentinel
-        self.head_sentinel.prev = None
-        self.tail_sentinel.next = None
-        self.tail_sentinel.prev = self.head_sentinel
+        self.header = self.Node()
+        self.trailer = self.Node()
+        self.header.next = self.trailer
+        self.header.prev = None
+        self.trailer.next = None
+        self.trailer.prev = self.header
 
     class Node(LinkedListBase.Node):
         def __init__(self):
@@ -20,59 +20,71 @@ class DoublyLinkedList(LinkedListBase):
     def add_first(self, data):
         new_node = self.Node()
         new_node.data = data
-        old_node = self.head_sentinel.next
+        old_node = self.header.next
         new_node.next = old_node
-        new_node.prev = self.head_sentinel
+        new_node.prev = self.header
         old_node.prev = new_node
-        self.head_sentinel.next = new_node
+        self.header.next = new_node
         self.len += 1
 
     def add_last(self, data):
         new_node = self.Node()
         new_node.data = data
-        old_node = self.tail_sentinel.prev
+        old_node = self.trailer.prev
         new_node.prev = old_node
-        new_node.next = self.tail_sentinel
+        new_node.next = self.trailer
         old_node.next = new_node
-        self.tail_sentinel.prev = new_node
+        self.trailer.prev = new_node
         self.len += 1
 
     def remove_last(self):
         if self.is_empty():
             raise IndexError
-        old_last = self.tail_sentinel.prev
+        old_last = self.trailer.prev
         new_last = old_last.prev
-        self.tail_sentinel.prev = new_last
-        new_last.next = self.tail_sentinel
+        self.trailer.prev = new_last
+        new_last.next = self.trailer
         self.len -= 1
         return old_last
 
     def remove_first(self):
         if self.is_empty():
             raise IndexError
-        old_first = self.head_sentinel.next
+        old_first = self.header.next
         new_first = old_first.next
-        self.head_sentinel.next = new_first
-        new_first.prev = self.head_sentinel
+        self.header.next = new_first
+        new_first.prev = self.header
         self.len -= 1
         return old_first
 
     def last(self):
         if self.is_empty():
             raise IndexError
-        return self.tail_sentinel.prev
+        return self.trailer.prev
 
 
 class DoublyLinkedTest(unittest.TestCase):
     def test_double(self):
-        doubly_linked = DoublyLinkedList()
-        doubly_linked.add_last("1")
-        self.assertEqual(len(doubly_linked), 1)
-        self.assertEqual(doubly_linked.last().data, "1")
-        self.assertEqual(doubly_linked.remove_last().data, "1")
-        self.assertEqual(doubly_linked.is_empty(), True)
+        double_list = DoublyLinkedList()
+        node1 = double_list.insert(double_list.header, double_list.trailer, "1")
+        self.assertEqual(len(double_list), 1)
+        self.assertEqual(double_list.last().data, "1")
+        self.assertEqual(double_list.first().data, "1")
+
+        node2 = double_list.insert(node1, double_list.trailer, "2")
+        self.assertEqual(double_list.last(), node2)
+
         with self.assertRaises(IndexError):
-            doubly_linked.remove_last()
+            double_list.insert(double_list.header, double_list.trailer, "3")
+
+        with self.assertRaises(IndexError):
+            double_list.remove(double_list.header, double_list.trailer)
+
+        self.assertEqual(double_list.remove(double_list.header, node2).data, "1")
+        self.assertEqual(len(double_list), 1)
+        self.assertEqual(double_list.remove(double_list.header, double_list.trailer).data, "2")
+        with self.assertRaises(IndexError):
+            double_list.remove(double_list.header, double_list.trailer)
 
 
 if __name__ == "__main__":
