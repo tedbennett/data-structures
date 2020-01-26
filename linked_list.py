df957b3bdefc76f730e74cf1d24ed49c3eb8f516
ted <teddsbennett@gmail.com>
2020-01-26 12:08:54 +0000

    reimplement as linked list

--- linked_list.py
+++ linked_list.py
@@ -1,33 +1,55 @@
 import unittest
 
 
-class Node:
-    def __init__(self, data=None, next=None):
-        self._node = next
-        self._data = data
+class LinkedList:
+    class Node:
+        def __init__(self, data=None, next=None):
+            self._next = next
+            self._data = data
 
-    def get_data(self):
-        return self._data
+    def __init__(self):
+        self.first = None
+        self.last = None
+        self.len = 0
 
-    def set_data(self, data):
-        self._data = data
+    def add_first(self, data):
+        new_first = self.Node(data)
+        if self.len == 0:
+            self.last = new_first
+        else:
+            new_first._next = self.first
+        self.first = new_first
+        self.len += 1
 
-    def next(self):
-        return self._node
+    def add_last(self, data):
+        new_last = self.Node(data)
+        if self.len == 0:
+            self.first = new_last
+        else:
+            self.last._next = new_last
+        self.last = new_last
+        self.len += 1
 
 
-class NodeTest(unittest.TestCase):
-    def test_node(self):
-        node = Node()  # create empty node
-        self.assertEqual(node.get_data(), None)  # check node data initialised correctly
-        node.set_data("hello")
-        self.assertEqual(node.get_data(), "hello")  # check set data
-        self.assertEqual(node.next(), None)
-
+class ListTest(unittest.TestCase):
     def test_list(self):
-        node1 = Node("1")
-        node2 = Node("2", node1)  # create list of 2 nodes, node2 pointing to node1
-        self.assertEqual(node2.next(), node1)
+        """
+        Testing adding and removing to the list
+        :return:
+        """
+        linked = LinkedList()  # create empty list
+        linked.add_last("1")
+        linked.add_last("2")
+        linked.add_first("3")
+        item = linked.first
+        for i in range(linked.len):
+            print(item._data)
+            item = item._next
+        self.assertEqual(linked.remove_first(), "2")
+        self.assertEqual(linked.remove_first(), "3")
+        self.assertEqual(linked.remove_first(), "1")
+        with self.assertRaises(IndexError):
+            linked.remove_first()
 
 
 if __name__ == "__main__":
