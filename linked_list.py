046394bd73c75d7430dd942dec5a56e480b7158e
ted <teddsbennett@gmail.com>
2020-01-26 11:29:13 +0000

    node initialisation unit test

--- linked_list.py
+++ linked_list.py
@@ -1,10 +1,29 @@
 import unittest
 
 
-class DequeTest(unittest.TestCase):
+class Node:
+    def __init__(self, data=None):
+        self._node = None
+        self._data = data
+
+    def get_data(self):
+        return self._data
+
+    def set_data(self, data):
+        self._data = data
+
+    def next(self):
+        return self._node
+
+
+class NodeTest(unittest.TestCase):
     def test_node(self):
         node = Node()  # create empty node
         self.assertEqual(node.get_data(), None)  # check node data initialised correctly
         node.set_data("hello")
         self.assertEqual(node.get_data(), "hello")  # check set data
         self.assertEqual(node.next(), None)
+
+
+if __name__ == "__main__":
+    unittest.main()
